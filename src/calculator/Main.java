43eba7f96eccc04f1896d053541ad95a4221d215
Kinga Wrona <kingawr123@gmail.com>
2018-10-03 19:07:43 +0200

    + trigonometry

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,31 +1,44 @@
 package calculator;
 
 import java.util.Scanner;
-import static java.lang.Math.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        double firstNumber = getNumber();
+        switch (chooseCalculat()){
+            case "z":
+                double firstNumber = getNumber();
 
-        String firstOperator = getOperator();
+                String firstOperator = getOperator();
 
-        double secondNumber = getNumber();
+                double secondNumber = getNumber();
 
-        String secondOperator = getOperator();
+                String secondOperator = getOperator();
 
-        double thirdNumber = getNumber();
+                double thirdNumber = getNumber();
 
-        try {
-            double firstResult = calculatResult(firstNumber, secondNumber, firstOperator);
-            double endResult = calculatResult(firstResult, thirdNumber, secondOperator);
+                try {
+                    double firstResult = calculatResult(firstNumber, secondNumber, firstOperator);
+                    double endResult = calculatResult(firstResult, thirdNumber, secondOperator);
 
-            System.out.println(endResult);
+                    System.out.println(endResult);
+                } catch (IllegalArgumentException exception) {
+                    System.out.println(exception.getMessage());
+                }
+                break;
+            case "t":
+                double angle = getAngle();
+                String trigonometryOperator = getTrigonometryOperator();
+                System.out.println(trigonometryResult(angle, trigonometryOperator));
+                break;
+                default:
+                    System.out.println("Error");
+                    break;
         }
-        catch(IllegalArgumentException exception) {
-            System.out.println(exception.getMessage());
-        }
+
     }
+
+
 
     public static double calculatResult(double firstNumber, double secondNumber, String operator) throws IllegalArgumentException {
 
@@ -41,23 +54,61 @@ public class Main {
             case "%":
                 return firstNumber % secondNumber;
             case "^":
-                return pow(firstNumber, secondNumber) ;
+                return Math.pow(firstNumber, secondNumber) ;
             default:
                 throw new IllegalArgumentException("Operator '"+ operator +"' jest zły");
         }
     }
 
+    public static double trigonometryResult(double number, String operator){
+
+        if ("sinus".equals(operator)) {
+            return Math.sin(number);
+        } else {
+            if ("cosinus".equals(operator)) {
+                return Math.cos(number);
+            } else {
+                return 0;
+            }
+        }
+    }
+
     private static double getNumber(){
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Podaj liczbę:");
         return scanner.nextDouble();
+    }
+
+    private static double getAngle(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj kąt:");
+        return scanner.nextDouble();
+    }
+
+    private static String getTrigonometryOperator(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("'s' = sinus; 'c' = cosinus");
+        System.out.println("Wybierz działanie:");
+        return scanner.next();
     }
 
     private static String getOperator (){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Wybierz działanie:");
         System.out.println("'+' = dodawanie; '-' = odejmowanie; '*' = mnożenie; '/' = dzielenie; '%' = reszta z dzielenia; '^' = potęgowanie");
+        System.out.println("Wybierz działanie:");
+
          return scanner.next();
+    }
+
+    private static String chooseCalculat(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Chesz zrobić działanie trygonometryczne, czy zwykłe?");
+        System.out.println("Trygonometryczne = 't', Zwykłe = 'z'");
+        return  scanner.next();
     }
 }
