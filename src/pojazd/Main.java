34399aa39881b6f6c8c798c2f56776e9860a926c
Kinga Wrona <kingawr123@gmail.com>
2018-11-14 20:02:54 +0100

    + pojazd

---
+++ src/pojazd/Main.java
@@ -0,0 +1,27 @@
+package pojazd;
+
+public class Main {
+    public static void main(String[] args) {
+        Pojazd pojazd = new Pojazd("Kinga Kruk");
+        przerywnik();
+        System.out.println(pojazd.toString());
+        pojazd.przyspiesz(60);
+        pojazd.wsiadzDoPojazdu();
+        przerywnik();
+        Pojazd pojazd2 = new Pojazd("Ola Kołderka");
+        przerywnik();
+        System.out.println(pojazd2.toString());
+        pojazd2.wsiadzDoPojazdu();
+        pojazd2.wysiadzZPojazdu();
+        pojazd2.wysiadzZPojazdu();
+        pojazd2.przyspiesz(160);
+        pojazd2.zwolnij(30);
+        pojazd2.zmienWlasciciela("Michał Olechnik");
+
+
+    }
+    private static void przerywnik(){
+        System.out.println("____________________________________________________________________________");
+        System.out.println("");
+    }
+}
---
+++ src/pojazd/Pojazd.java
@@ -0,0 +1,87 @@
+package pojazd;
+
+public class Pojazd {
+    private int obecnaPredkosc = 0;
+    private static int maxPredkosc;
+    private static int maxIloscPasazerow;
+    private int obecnaIloscPasazerow = 0;
+    private String imieINazwiskoWlasciciela;
+
+    static {
+        maxPredkosc = 150;
+        maxIloscPasazerow = 6;
+    }
+
+    Pojazd(String imieINazwiskoWlasciciela){
+        this.imieINazwiskoWlasciciela = imieINazwiskoWlasciciela;
+        wyswietlMaxPredkosc();
+        wyswietlMaxIloscPasazerow();
+    }
+
+    public void przyspiesz(int p){
+        if (p<=maxPredkosc-obecnaPredkosc){
+            obecnaPredkosc = obecnaPredkosc + p;
+            System.out.println("Przyśpieszyłeś o "+p+" km/h");
+            wyswietlPredkosc();
+        }
+        else{
+            obecnaPredkosc = maxPredkosc;
+            System.out.println("Nie możesz przekraczać prędkości!");
+            wyswietlPredkosc();
+        }
+    }
+
+    public void zwolnij(int z){
+        if (z>obecnaPredkosc){
+            obecnaPredkosc = 0;
+            System.out.println("Zatrzymałeś się");
+        }
+        else{
+            obecnaPredkosc -= z;
+            System.out.println("Zwolniłeś o "+z+ " km/h");
+            wyswietlPredkosc();
+        }
+    }
+
+    public void wsiadzDoPojazdu(){
+        if (obecnaIloscPasazerow < maxIloscPasazerow){
+            obecnaIloscPasazerow++;
+            wyswietlLiczbePasazerow();
+        }
+        else System.out.println("Nie można wsiąść do pojazdu. Za dużo pasażerów!");
+    }
+
+    public void wysiadzZPojazdu(){
+        if (obecnaIloscPasazerow>0){
+            obecnaIloscPasazerow--;
+            wyswietlLiczbePasazerow();
+        }
+        else System.out.println("Nikogo nie ma w pojeździe!");
+    }
+
+    public void zmienWlasciciela(String imieINazwiskoWlasciciela){
+        this.imieINazwiskoWlasciciela = imieINazwiskoWlasciciela;
+        wyswietlImieINazwiskoWlasciciela();
+    }
+
+    private void wyswietlPredkosc(){
+        System.out.println("Prędkość: "+obecnaPredkosc+" km/h");
+    }
+    private void wyswietlLiczbePasazerow(){
+        System.out.println("liczba Pasażerów: "+obecnaIloscPasazerow);
+    }
+    private void wyswietlImieINazwiskoWlasciciela(){
+        System.out.println("Właściciel: "+imieINazwiskoWlasciciela);
+    }
+    private void wyswietlMaxPredkosc(){ System.out.println("Maksymalna prędkość pojazdu: " + maxPredkosc +"km/h"); }
+    private void wyswietlMaxIloscPasazerow(){System.out.println("Maksymalna ilość pasażerów: "+ maxIloscPasazerow);}
+
+    @Override
+    public String toString() {
+        return "Pojazd:{" +
+                "obecnaPredkosc=" + obecnaPredkosc + " km/h"+
+                ", obecnaIloscPasazerow=" + obecnaIloscPasazerow +
+                ", imieINazwiskoWlasciciela='" + imieINazwiskoWlasciciela + '\'' +
+                '}';
+    }
+}
